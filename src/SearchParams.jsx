129145aadb1ad0e0e6246f2131b821ca9fe133cb
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-07-16 07:44:19 +0545

    Component composition - separated Results from searchparams into Results.jxs

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
+import Results from "./Results";
 import useBreedList from "./useBreedList";
 const SearchParams = () => {
   const [location, setLocation] = useState("");
@@ -68,14 +68,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
