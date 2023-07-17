6298348a2a0168e2435ce781cda6a414c8eb1f37
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-07-17 20:07:14 +0545

    refactored fetchBreed and useBreedList to use query

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,29 @@ import SearchParams from "./SearchParams";
 import Details from "./Details";
 import { Link } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </div>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
