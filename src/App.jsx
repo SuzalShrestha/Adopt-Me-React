e261d74eab6a826903f7d9b4e07e447c462f79d1
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-07-17 08:16:03 +0545

    added details page and react router and link in pets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import SearchParams from './SearchParams';  
-  const App = () => {
-   return (<div>
+import React from "react";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
         <h1>Adopt Me!</h1>
-        <SearchParams />
-    </div>)
-  };
-  const container = document.getElementById("root");
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+const container = document.getElementById("root");
 const root = createRoot(container);
-  root.render(<App />);
+root.render(<App />);
---
+++ src/Details.jsx
@@ -0,0 +1,9 @@
+const Details = (props) => {
+  return (
+    <div>
+      <h1>Details Page</h1>
+      <p>Details Page</p>
+    </div>
+  );
+};
+export default Details;
